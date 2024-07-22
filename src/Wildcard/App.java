542edd62320bdfa1c7e962f7ac25f9b7bfd7cbb5
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2024-07-22 22:25:19 +0700

    master

--- src/Wildcard/App.java
+++ src/Wildcard/App.java
@@ -7,8 +7,9 @@ class App {
 
   public static void main(String[] args) {
     String filter = ip.nextLine();
+    int length = ip.nextInt();
 
-    String[] Filters = new String[ip.nextInt()];
+    String[] Filters = new String[length];
     for (int i = 0; i <= Filters.length; i++) {
       Filters[i] = ip.nextLine();
     }
